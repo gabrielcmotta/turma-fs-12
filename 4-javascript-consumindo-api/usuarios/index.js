3c4ce4e6099e93ce43fe03915ee5c482af1f4407
Eduardo Roseo <eduardoroseo@gmail.com>
2024-05-04 17:01:42 -0300

    feat: incluindo upload de imagens

--- 4-javascript-consumindo-api/usuarios/index.js
+++ 4-javascript-consumindo-api/usuarios/index.js
@@ -9,7 +9,10 @@ function montaTabela() {
             dadosUsuarios.map(function(usuario) {
                 document.getElementById('tabela-de-usuarios').innerHTML += `
                     <tr>
-                        <td>${usuario.id}</td>
+                        <td class="d-flex justify-content-center">
+                            <img class="img-icon" src="${usuario.image}" />
+                        </td>
+                        <td >${usuario.id}</td>
                         <td>${usuario.name}</td>
                         <td>${usuario.email}</td>
                         <td>
@@ -81,6 +84,7 @@ document.addEventListener('DOMContentLoaded', function() {
 document.getElementById("btn-cadastrar-usuario").addEventListener("click", function() {
     let inputNome = document.getElementById('nome').value
     let inputEmail = document.getElementById('email').value
+    let inputImage = document.getElementById('image').files[0]
 
     if (!inputNome) {
         alert('Preencha o nome')
@@ -91,46 +95,54 @@ document.getElementById("btn-cadastrar-usuario").addEventListener("click", funct
         alert('Preencha o email')
         return
     }
+    // convert image to base64
+    let reader = new FileReader()
+    reader.onload = function() {
+        let base64 = reader.result
 
-    let body = {
-        name: inputNome,
-        email: inputEmail
-    }
-
-    if (editID != null) {
-        fetch(`http://localhost:3000/usuarios/${editID}`, {
-            method: 'PUT',
+        let body = {
+            name: inputNome,
+            email: inputEmail,
+            image: base64
+        }
+    
+        if (editID != null) {
+            fetch(`http://localhost:3000/usuarios/${editID}`, {
+                method: 'PUT',
+                body: JSON.stringify(body)
+            })
+                .then(resposta => resposta.json())
+                .then(usuario => {
+                    montaTabela()
+                    limparFormulario()
+                    document.getElementById('btn-cadastrar-usuario').innerText = "Cadastrar"
+                    editID = null
+                    Swal.fire({
+                        title: 'Atualizado com sucesso',
+                        icon: 'success',
+                        timer: 2000,
+                        timerProgressBar: true,
+                    })
+                })
+            return;
+        }
+    
+        fetch(`http://localhost:3000/usuarios`, {
+            method: 'POST',
             body: JSON.stringify(body)
         })
             .then(resposta => resposta.json())
             .then(usuario => {
-                montaTabela()
-                limparFormulario()
-                document.getElementById('btn-cadastrar-usuario').innerText = "Cadastrar"
-                editID = null
                 Swal.fire({
-                    title: 'Atualizado com sucesso',
+                    title: 'Cadastrado com sucesso',
                     icon: 'success',
                     timer: 2000,
                     timerProgressBar: true,
                 })
+                montaTabela()
+                limparFormulario()
             })
-        return;
     }
 
-    fetch(`http://localhost:3000/usuarios`, {
-        method: 'POST',
-        body: JSON.stringify(body)
-    })
-        .then(resposta => resposta.json())
-        .then(usuario => {
-            Swal.fire({
-                title: 'Cadastrado com sucesso',
-                icon: 'success',
-                timer: 2000,
-                timerProgressBar: true,
-            })
-            montaTabela()
-            limparFormulario()
-        })
+    reader.readAsDataURL(inputImage)
 })
