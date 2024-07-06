cd90dfa6331944945cf29019be20635f68c64fdf
Eduardo Roseo <eduardoroseo@gmail.com>
2024-07-06 16:10:43 -0300

    feat: instalando react hook form

--- 7-react/aula-react/src/components/Cabecalho/index.tsx
+++ 7-react/aula-react/src/components/Cabecalho/index.tsx
@@ -1,16 +1,11 @@
-import { useUsuario } from "../../contexts/UsuarioProvider";
-
 type CabecalhoProps = {
   titulo: string;
 };
 
 function Cabecalho(props: CabecalhoProps) {
-  const usuarioContext = useUsuario();
-
   return (
     <div className="row text-center">
       <h1>{props.titulo}</h1>
-      <h3>Quantidade de usuários atual: {usuarioContext.qtdUsuarios}</h3>
     </div>
   );
 }
--- 7-react/aula-react/src/components/Form/index.tsx
+++ 7-react/aula-react/src/components/Form/index.tsx
@@ -1,56 +1,88 @@
-import { useState } from "react";
 import { useUsuario } from "../../contexts/UsuarioProvider";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+const schema = yup.object({
+  nome: yup
+    .string()
+    .required("O campo é requerido")
+    .min(3, "O nome deve conter pelo menos 3 caracteres")
+    .max(20, "O nome deve conter no máximo 20 caracteres")
+    .trim(),
+  email: yup
+    .string()
+    .email("O campo deve ser um email válido")
+    .required("O campo é requerido")
+    .trim(),
+});
+
+type FormData = {
+  nome: string;
+  email: string;
+};
 
 function Form() {
-  const [nome, setNome] = useState("");
-  const [email, setEmail] = useState("");
-
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<FormData>({
+    resolver: yupResolver(schema),
+  });
   const { qtdUsuarios, salvarUsuario } = useUsuario();
 
-  const enviarFormulario = async function () {
-    const novoUsuario: Usuario = {
-      nome,
-      email,
-    };
+  const enviarFormulario = async function (data: FormData) {
+    await salvarUsuario(data);
 
-    salvarUsuario(novoUsuario);
-
-    setNome("");
-    setEmail("");
+    reset();
   };
 
   return (
     <>
-      <div className="row">
-        <div className="col">
-          <label className="form-label">Nome</label>
-          <input
-            value={nome}
-            onChange={(evento) => setNome(evento.target.value)}
-            placeholder="Nome Teste"
-            type="text"
-            className="form-control"
-          />
+      <form onSubmit={handleSubmit(enviarFormulario)}>
+        <div className="row">
+          <div className="col">
+            <label className="form-label">Nome</label>
+            <input
+              {...register("nome")}
+              placeholder="Nome Teste"
+              className={
+                errors?.nome?.message
+                  ? "form-control is-invalid"
+                  : "form-control"
+              }
+            />
+            {errors?.nome?.message && (
+              <div className="invalid-feedback">{errors?.nome?.message}</div>
+            )}
+          </div>
+          <div className="col">
+            <label className="form-label">Email</label>
+            <input
+              placeholder="email@teste.com"
+              {...register("email")}
+              className={
+                errors?.email?.message
+                  ? "form-control is-invalid"
+                  : "form-control"
+              }
+            />
+            {errors?.email?.message && (
+              <div className="invalid-feedback">{errors?.email?.message}</div>
+            )}
+          </div>
+          <div className="col d-flex align-items-end">
+            <button type="submit" className="btn btn-primary">
+              Salvar
+            </button>
+          </div>
+          <div className="col d-flex align-items-end">
+            <span>Qtd Usuários: {qtdUsuarios}</span>
+          </div>
         </div>
-        <div className="col">
-          <label className="form-label">Email</label>
-          <input
-            placeholder="email@teste.com"
-            value={email}
-            onChange={(evento) => setEmail(evento.target.value)}
-            type="email"
-            className="form-control"
-          />
-        </div>
-        <div className="col d-flex align-items-end">
-          <button onClick={enviarFormulario} className="btn btn-primary">
-            Salvar
-          </button>
-        </div>
-        <div className="col d-flex align-items-end">
-          <span>Qtd Usuários: {qtdUsuarios}</span>
-        </div>
-      </div>
+      </form>
     </>
   );
 }
