88437e2aa4969b0eedb3da34b1ad12b4c98a18b7
Eduardo Roseo <eduardoroseo@gmail.com>
2024-08-24 20:01:03 +0000

    feat: Adicionar arquivo de usuários e rota para exibir usuários

---
+++ 11-nodejs/src/usuarios.ts
@@ -0,0 +1,17 @@
+export const usuarios = [
+  {
+    id: 1,
+    name: "Eduardo",
+    email: "eduardo@gmail.com",
+  },
+  {
+    id: 2,
+    name: "Maria",
+    email: "maria@gmail.com",
+  },
+  {
+    id: 3,
+    name: "Jose",
+    email: "jose@gmail.com",
+  },
+];
