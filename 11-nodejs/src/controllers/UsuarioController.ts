5543b25c8e3a3b6ee64a910d57cbeec30daf0044
Eduardo Roseo <eduardoroseo@gmail.com>
2024-09-21 17:42:52 +0000

    Refatorar controlador de usuários e adicionar rotas para exibir, criar e atualizar usuários

--- 11-nodejs/src/controllers/UsuarioController.ts
+++ 11-nodejs/src/controllers/UsuarioController.ts
@@ -1,22 +1,85 @@
 import { Request, Response } from "express";
 import { usuarios } from "../usuarios";
 
+type UsuarioRequestDTO = {
+  name: string;
+  email: string;
+};
+
 export class UsuarioController {
-    async getUsuarios(req: Request, res: Response) {
-        return res.send(usuarios).json();
+  async getUsuarios(req: Request, res: Response) {
+    return res.send(usuarios).json();
+  }
+
+  async getUsuario(req: Request, res: Response) {
+    const { id } = req.params;
+
+    const resultado = usuarios.filter((user) => user.id == id);
+
+    if (resultado.length == 0) {
+      return res.status(404).send({ error: "Usuário não encontrado" }).json();
     }
 
-    async getUsuario(req: Request, res: Response) {
-        const { id } = req.params
-    
-        const resultado = usuarios.filter(user => user.id == id)
-    
-        if (resultado.length == 0) {
-          return res.status(404)
-            .send({ error: "Usuário não encontrado"})
-            .json()
-        }
-    
-        return res.send(resultado[0]).json()
+    return res.send(resultado[0]).json();
+  }
+
+  async createUsuario(req: Request, res: Response) {
+    const dados: UsuarioRequestDTO = req.body;
+
+    if (!dados.email) {
+      return res
+        .status(422)
+        .send({ error: "O campo email é obrigatório!" })
+        .json();
     }
+
+    if (!dados.name) {
+      return res
+        .status(422)
+        .send({ error: "O campo nome é obrigatório!" })
+        .json();
+    }
+
+    const novoUsuario = {
+      ...dados,
+      id: Math.random().toString(),
+    };
+
+    usuarios.push(novoUsuario);
+
+    return res.status(201).send(novoUsuario).json();
+  }
+
+  async updateUsuario(req: Request, res: Response) {
+    const { id } = req.body;
+    const dados: UsuarioRequestDTO = req.body;
+
+    if (!dados.email) {
+      return res
+        .status(422)
+        .send({ error: "O campo email é obrigatório!" })
+        .json();
+    }
+
+    if (!dados.name) {
+      return res
+        .status(422)
+        .send({ error: "O campo nome é obrigatório!" })
+        .json();
+    }
+
+    const indiceUsuario = usuarios.findIndex((user) => user.id == id);
+
+    if (indiceUsuario < 0) {
+      return res.status(404).send({ error: "Usuário não encontrado" }).json();
+    }
+
+    usuarios[indiceUsuario] = {
+      ...usuarios[indiceUsuario],
+      name: dados.name,
+      email: dados.email,
+    };
+
+    res.send(usuarios[indiceUsuario]).json();
+  }
 }
--- 11-nodejs/src/routes/usuarioRoutes.ts
+++ 11-nodejs/src/routes/usuarioRoutes.ts
@@ -1,11 +1,12 @@
 import { Request, Response, Router } from "express";
 import { UsuarioController } from "../controllers/UsuarioController";
-import { usuarios } from "../usuarios";
 
 const usuarioRoutes = Router();
 const usuarioController = new UsuarioController();
 
 usuarioRoutes.get("/usuarios", usuarioController.getUsuarios);
 usuarioRoutes.get("/usuarios/:id", usuarioController.getUsuario);
+usuarioRoutes.post("/usuarios", usuarioController.createUsuario);
+usuarioRoutes.put("/usuarios/:id", usuarioController.updateUsuario);
 
 export default usuarioRoutes;
